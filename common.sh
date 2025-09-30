fd209ee251c3350c084b1b6b25e52ffcf368b883
GorleJansi <gorlejansi2001@gmail.com>
2025-09-30 14:04:30 +0530

    all

--- common.sh
+++ common.sh
@@ -15,7 +15,9 @@ MYSQL_HOST=mysql.jansi1.site
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
-check_root(){
+
+
+check_root(){ # check if script running with root privilages  
     if [ $USERID -ne 0 ]; then
         echo "ERROR:: Please run this script with root privelege"
         exit 1 # failure is other than 0
@@ -23,7 +25,7 @@ check_root(){
 }
 
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
+VALIDATE(){ # functions receive inputs through args just like shell script args-validate commands
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
@@ -32,19 +34,19 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
     fi
 }
 
-nodejs_setup(){
+
+nodejs_setup(){ # install nodejs,install dependencies
     dnf module disable nodejs -y &>>$LOG_FILE
     VALIDATE $? "Disabling NodeJS"
     dnf module enable nodejs:20 -y  &>>$LOG_FILE
     VALIDATE $? "Enabling NodeJS 20"
     dnf install nodejs -y &>>$LOG_FILE
     VALIDATE $? "Installing NodeJS"
-
     npm install &>>$LOG_FILE
     VALIDATE $? "Install dependencies"
 }
 
-java_setup(){
+java_setup(){ # install java,install dependencies
     dnf install maven -y &>>$LOG_FILE
     VALIDATE $? "Installing Maven"
     mvn clean package &>>$LOG_FILE
@@ -53,7 +55,7 @@ java_setup(){
     VALIDATE $? "Renaming the artifact"
 }
 
-python_setup(){
+python_setup(){ # install python,install dependencies
     dnf install python3 gcc python3-devel -y &>>$LOG_FILE
     VALIDATE $? "Installing Python3"
     pip3 install -r requirements.txt &>>$LOG_FILE
@@ -61,30 +63,31 @@ python_setup(){
 }
 
 app_setup(){
-    id roboshop &>>$LOG_FILE
+    # user-create
     if [ $? -ne 0 ]; then
         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
         VALIDATE $? "Creating system user"
     else
         echo -e "User already exist ... $Y SKIPPING $N"
     fi
+    # user-home dir
     mkdir -p /app
     VALIDATE $? "Creating app directory"
-
+    # download app code to tmp
     curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
     VALIDATE $? "Downloading $app_name application"
-
+    # go to  app dir
     cd /app 
     VALIDATE $? "Changing to app directory"
-
+    # remove old code
     rm -rf /app/*
     VALIDATE $? "Removing existing code"
-
+    # download app code in app dir
     unzip /tmp/$app_name.zip &>>$LOG_FILE
     VALIDATE $? "unzip $app_name"
 }
 
-systemd_setup(){
+systemd_setup(){  # service added in systemd,enable,start
     cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service
     VALIDATE $? "Copy systemctl service"
 
@@ -93,7 +96,7 @@ systemd_setup(){
     VALIDATE $? "Enable $app_name"
 }
 
-app_restart(){
+app_restart(){ #restart at end after all
     systemctl restart $app_name
     VALIDATE $? "Restarted $app_name"
 }
